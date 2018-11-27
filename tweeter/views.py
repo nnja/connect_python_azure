1d4c0e89f899499a29774a2c5921b269b6434943
Nina Zakharenko <nzakharenko@gmail.com>
2018-11-26 16:57:09 -0800

    Update permissions

--- tweeter/views.py
+++ tweeter/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from tweeter.models import Tweet, User
-from tweeter.permissions import IsAuthorOrReadOnly
+from tweeter.permissions import IsAuthorOrReadOnly, IsSelfOrAdmin
 from tweeter.serializers import TweetSerializer, UserSerializer
 
 from django.contrib.auth import authenticate, login
@@ -20,13 +20,13 @@ def index(request):
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAdminUser]
+    permission_classes = [IsSelfOrAdmin]
 
 
 class TweetViewSet(viewsets.ModelViewSet):
     queryset = Tweet.objects.all()
     serializer_class = TweetSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    permission_classes = [IsAuthorOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
